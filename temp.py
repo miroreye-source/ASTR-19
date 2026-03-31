08c85b95982fd7aa453a89d5d93ad5c237d3bd3e
miroreye-source <miroreye@ucsc.edu>
2026-03-31 13:06:05 -0700

    uploaded a file
    uploaded the temp.py file

---
+++ temp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def PrintHelloWorld():
+    print("Hello World!")
+    
+def main():
+    PrintHelloWorld()
+    
+    
+if __name__ == "__main__":
+    main()
