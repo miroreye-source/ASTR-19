2faff9b6133e9654de9c8090cccc170c8a2a3e7e
miroreye-source <miroreye@ucsc.edu>
2026-04-06 16:20:58 -0700

    Added about_me.py file

---
+++ about_me.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:17:50 2026
+
+@author: miloreyes
+"""
+
+def about_me():
+    print("My name is Milo Reyes")
+    print("My preferred pronouns are he/him")
+    print("My favorite movie is Interstellar")
+    print("My favorite food is pizza")
+    
+about_me()
