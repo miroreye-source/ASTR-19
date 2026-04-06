e61e2bab35657b5789c88c5411dcc8d768afef1d
miroreye-source <miroreye@ucsc.edu>
2026-04-06 16:32:23 -0700

    Added math-functions.py file

---
+++ math-functions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:22:13 2026
+
+@author: miloreyes
+"""
+
+def sum(float1, float2):
+    print(f"The sum of {float1} and {float2} is {float1 + float2}")
+    print(type(float1+float2))
+
+
+def difference(num1, num2):
+    print(f"The difference between {num1} and {num2} is {num2-num1}")
+    print(type(num2-num1))
+    
+
+def product(float1, num1):
+    print(f"The product of {float1} and {num1} is {float1*num1}")
+    print(type(float1*num1))
+
+def main():
+    sum(5.0, 6.0)
+    difference(5, 6)
+    product(5.0, 6)
+    
+main()
